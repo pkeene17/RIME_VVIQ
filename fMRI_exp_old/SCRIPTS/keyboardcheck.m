245a0d5e69f4b9b9ba29e7c276669863b9be1634
Kuhl Lab Manager <kuhllabmanager@dyn-184-171-85-84.uoregon.edu>
2022-04-07 15:17:38 -0700

    remove_dirs

--- fMRI_exp_old/SCRIPTS/keyboardcheck.m
+++
@@ -1,8 +0,0 @@
-keyDown = 0; 
-while 1
-                [keyDown, secs, keyCodes] = KbCheck(-1); %checks for responses from all KBs
-                keyDown
-                if keyDrtown == 1
-                    break;
-                end
- end
--- fMRI_exp_old/SCRIPTS/oddevenblockassign.m
+++
@@ -1,5 +0,0 @@
-oddevenblock = [1 2 1 2 1 2 1 2 1 2 1 2 1 2 1 2];
-
-save oddevenblock
-
-theData.Study1.miniblock
