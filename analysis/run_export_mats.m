862b7177e294605378b5edcfda1cf297a585548b
Kuhl Lab Manager <kuhllabmanager@dyn-184-171-85-84.uoregon.edu>
2022-05-24 15:08:12 -0700

    n=32

--- analysis/run_export_mats.m
+++ analysis/run_export_mats.m
@@ -2,12 +2,10 @@
 function run_export_mats(nsubj)
 
 for i=1:nsubj
-    if i~=18
-        if i<10
-            subj=strcat('0',int2str(i));
-        else
-            subj=int2str(i);
-        end
-        export_mats(subj)
+    if i<10
+        subj=strcat('0',int2str(i));
+    else
+        subj=int2str(i);
     end
+    export_mats(subj)
 end
